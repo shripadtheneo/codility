93a529c15094f046291bf3f7c861edf087903080
shripadtheno <shripadtheneo@gmail.com>
2017-01-20 14:02:10 +0900

    Initial commit for Increasing Value Part Array (successive)

---
+++ value_part_array/inc_val_part_arr.py
@@ -0,0 +1,29 @@
+"""
+Assume the input is an array of numbers 0 thru to 9. Here, an "Increasing Value Part Array" means
+a part of an array in which successive numbers are increasing.
+
+For example, "12", "49" and "678" are all "Increasing Value Part Arrays" but "53" and "22" are not.
+
+Given Above, implement a program to return the longest "Increasing Value Part Array" for any array
+input. e.g. "134297381"
+"""
+
+
+def long_inc_part_arr(A):
+    length = len(A)
+    final_arr = []
+    inter_arr = [str(A[0])]
+    for i in range(length - 1):
+        if A[i] < A[i+1]:
+            inter_arr.append(str(A[i+1]))
+        else:
+            if len(final_arr) < len(inter_arr):
+                final_arr = inter_arr
+            inter_arr = [str(A[i + 1])]
+
+    return final_arr
+
+
+if __name__ == '__main__':
+    A = [1, 3, 4, 2, 9, 7, 3, 8, 1]
+    print (long_inc_part_arr(A))
