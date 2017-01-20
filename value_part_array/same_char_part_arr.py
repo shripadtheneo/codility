6be8507ea5da253ef6d997eb59b32c96279df17f
shripadtheno <shripadtheneo@gmail.com>
2017-01-20 18:11:51 +0900

    Note for self and update for java

--- value_part_array/same_char_part_arr.py
+++ value_part_array/same_char_part_arr.py
@@ -30,3 +30,19 @@ def long_same_part_arr(sequence):
 if __name__ == '__main__':
     sequence = "AaBBBcC"
     print (long_same_part_arr(sequence))
+
+"""
+Note:
+    This program is solved using the Dynamic Programming approach. First concentration was on the
+    time complexity so need to traverse the loop only once causing us to make sure that we keep the
+    result stored result in my first python function which is commented out. Result was time
+    complexity to be O(n). (Since there is only once looping though the sequence as well as other have
+    constant time so time complexity is O(n)). Then there was need of reducing the space complexity so
+    rather than using two list, 2 array of size just 2 are used reducing the space complexity too.
+
+    Now for how i used the reusability and extendability is that for last 2 questions, code is same and
+    haven't changed at all while only the condition is changed between first part of code and second part
+    of code. As python is not strictly typed language it is possible in python but to do it in Java i
+    will need to make use of character array with type casting at the time of condition checking.
+    (Will code that later)
+"""
